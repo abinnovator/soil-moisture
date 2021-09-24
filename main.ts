3755986d41b1de80b631cae6f6bcc00f5a4e0c2c
abinnovator <89593957+abinnovator@users.noreply.github.com>
2021-09-24 15:43:16 +0530

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,6 +9,14 @@ if (input.buttonIsPressed(Button.A)) {
     basic.showNumber(reading)
 }
 basic.pause(5000)
+if (reading < 500) {
+    basic.showIcon(IconNames.Umbrella)
+    pins.servoWritePin(AnalogPin.P2, 0)
+    basic.pause(3000)
+    pins.servoWritePin(AnalogPin.P2, 80)
+    basic.pause(3000)
+    pins.servoWritePin(AnalogPin.P2, 0)
+}
 basic.forever(function () {
-	
+    led.setBrightness(64)
 })
